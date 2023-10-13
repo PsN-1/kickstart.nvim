63b567198b295b61d406968ded7cf2bf70fa996e
Pedro Neto <pedro.santosnt@hotmail.com>
2023-10-13 00:27:47 -0300

    add additional config for swift source-kit

--- init.lua
+++ init.lua
@@ -453,6 +453,24 @@ require 'lspconfig'.sourcekit.setup {
   on_attach = on_attach,
   capabilities = capabilities,
 }
+
+local swift_lsp = vim.api.nvim_create_augroup("swift_lsp", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "swift" },
+  callback = function()
+    local root_dir = vim.fs.dirname(vim.fs.find({
+      "Package.swift",
+      ".git",
+    }, { upward = true })[1])
+    local client = vim.lsp.start({
+      name = "sourcekit-lsp",
+      cmd = { "sourcekit-lsp" },
+      root_dir = root_dir,
+    })
+    vim.lsp.buf_attach_client(0, client)
+  end,
+  group = swift_lsp,
+})
 --
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
