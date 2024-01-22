c78fcb896126abd791bad7a803e7aa55aecb22e3
Pedro Neto <pedroneto@Pedros-MacBook-Pro.local>
2024-01-22 19:59:37 -0300

    update blank line

--- init.lua
+++ init.lua
@@ -63,15 +63,15 @@ require('lazy').setup({
   'lewis6991/gitsigns.nvim',
   -- 'github/copilot.vim',
 
-  'navarasu/onedark.nvim',               -- Theme inspired by Atom
+  'navarasu/onedark.nvim',     -- Theme inspired by Atom
   'catppuccin/nvim',
-  'nvim-lualine/lualine.nvim',           -- Fancier statusline
-  'lukas-reineke/indent-blankline.nvim', -- Add indentation guides even on blank lines
-  'numToStr/Comment.nvim',               -- "gc" to comment visual regions/lines
-  'tpope/vim-sleuth',                    -- Detect tabstop and shiftwidth automatically
+  'nvim-lualine/lualine.nvim', -- Fancier statusline
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl",     opts = {} },
+  'numToStr/Comment.nvim',     -- "gc" to comment visual regions/lines
+  'tpope/vim-sleuth',          -- Detect tabstop and shiftwidth automatically
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'nvim-telescope/telescope.nvim',       branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   {
@@ -160,13 +160,6 @@ require('lualine').setup {
 
 -- Enable Comment.nvim
 require('Comment').setup()
-
--- Enable `lukas-reineke/indent-blankline.nvim`
--- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
-}
 
 -- Gitsigns
 -- See `:help gitsigns.txt`
@@ -321,7 +314,7 @@ local servers = {
   gopls = {},
   pyright = {},
   tailwindcss = {},
-  -- tsserver = {},
+  tsserver = {},
   -- rust_analyzer = {},
   lua_ls = {
     Lua = {
